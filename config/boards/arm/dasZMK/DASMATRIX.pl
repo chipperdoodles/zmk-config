a0acb79171a4ec7467f15bc3a11f154d8ace16e9
chipperdoodles <myknobel@gmail.com>
2021-10-20 21:20:17 -0600

    initial daszmk

---
+++ config/boards/arm/dasZMK/DASMATRIX.pl
@@ -0,0 +1,21 @@
+    1 	    2 	    3 	    4 	    5 	    6 	    7 	    8 	    9 	    10 	    13 	    14 	    15 	    20 	    23 	    24 	    25 	    26
+11 	F6 	    LALT 	        H 	    G 	    F4 		        Esc 		    F11 	0_P 	'" 	    Spc 	._P 				            UP
+12 	8 	    PrtSC 	9 	    7 	    4 	    3 	    2 	    1 	    F5 	    F10 		    0 		PGD 				                    End
+16 	=+ 	            F8 	    6 	    5 	    F2 	    F1 	    ~ 	    LCTL 	F9 	    Ins 	-_ 	    Delete 	PgU 				            Home
+17 	K 	            L 	    J 	    F 	    D 	    S 	    A 		        Bkslsh 	2_P 	;: 	    1_P 	3_P     RWIN 		    RSHFT 	Enter_P
+18 	]} 	            F7 	    Y 	    T 	    F3 	    Caps 	Tab 		    Bkspc 	5_P 	[{ 	    4_P 	6_P 	LWIN 	        LSHFT	
+19 	I 	    Sclk 	O 	    U 	    R 	    E 	    W 	    Q 	    Pause 		    8_P 	P 	    7_P 	9_P 				            +_P
+21 	,< 	            .> 	    M 	    V 	    C 	    X 	    Z 	    RCTL 	Entr 	/_P 		    Num 	*_P 				
+22 	        RALT 	Apps 	N 	    B 					                    F12 	Right 	/? 	    Down 	-_P 				            Left
+
+https://www.reddit.com/r/MechanicalKeyboards/wiki/keyboard_matrices
+
+    0       1 	    2 	    3 	    4 	    5 	    6 	    7 	    8 	    9 	    10 	    11 	    12 	    13	    14 	    15 	    16 	    17
+0 	F6 	    LALT 	        H 	    G 	    F4 		        Esc 		    F11 	0_P 	'" 	    Spc 	._P 				            UP
+1 	8 	    PrtSC 	9 	    7 	    4 	    3 	    2 	    1 	    F5 	    F10 		    0 		PGD 				                    End
+2 	=+ 	            F8 	    6 	    5 	    F2 	    F1 	    ~ 	    LCTL 	F9 	    Ins 	-_ 	    Delete 	PgU 				            Home
+3 	K 	            L 	    J 	    F 	    D 	    S 	    A 		        Bkslsh 	2_P 	;: 	    1_P 	3_P     RWIN 		    RSHFT 	Enter_P
+4 	]} 	            F7 	    Y 	    T 	    F3 	    Caps 	Tab 		    Bkspc 	5_P 	[{ 	    4_P 	6_P 	LWIN 	        LSHFT	
+5 	I 	    Sclk 	O 	    U 	    R 	    E 	    W 	    Q 	    Pause 		    8_P 	P 	    7_P 	9_P 				            +_P
+6 	,< 	            .> 	    M 	    V 	    C 	    X 	    Z 	    RCTL 	Entr 	/_P 		    Num 	*_P 				
+7 	        RALT 	Apps 	N 	    B 					                    F12 	Right 	/? 	    Down 	-_P 				            Left
---
+++ config/boards/arm/dasZMK/README.md
@@ -0,0 +1,9 @@
+# Plapple340
+
+
+The Plapple340 is a hotswap 79 key keyboard made by Chipperdoodles. 
+
+## Building ZMK firmware
+```
+west build -p -b plapple340
+```
